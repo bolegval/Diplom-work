8b4edab7ab23925461e3bb78b722d89d2df5d6f0
bolegval <bondarenko_oleg@list.ru>
2019-11-13 14:11:41 +0300

    master

--- mail.php
+++ mail.php
@@ -1,33 +1,16 @@
 <?php
-
-$c = true;
-
-$callback = trim($_POST["Обратный звонок"]);
-$admin_email = trim($_POST["bondarenko_oleg@list.ru"]);
-$name = trim ($_POST["Имя"]);
-
-  foreach ( $_POST as $key => $value ) {
-    if ( $value != "" && $key != "callback" && $key != "admin_email" && $key != "name" ) {
-      $message .= "
-      " . ( ($c = !$c) ? '<tr>':'<tr>' ). "
-          <td style='padding: 10px; width: auto;'><b>$key:</b></td>
-          <td style='padding: 10px; width: 100%;'>$value</td>
-      </tr>
-      ";
-    }
-  }
-
-$message = "<table style='width: 50%;'>$message</table>";
-
-function adopt($text) {
-  return '=?UTF-8?B?'.Base64_enocode($text). '?=';
+$to = "bondarenko_oleg@list.ru"; 
+$tema = "Форма обратной связи на PHP"; 
+$message = "Ваше имя: ".$_POST['name']."<br>";
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; 
+  
+if (mail($to, $tema, $message, $headers)) {
+  echo ('Успешно отправлено!');
 }
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type; text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($callback).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-
-mail($admin_email, adopt($name), $message, $headers );
+else {
+  echo ('Ошибка!');
+}
 
 ?>
